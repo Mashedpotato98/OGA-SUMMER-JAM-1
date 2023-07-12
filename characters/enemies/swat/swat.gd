629668fd7ddf678ce7257fe4025e522fd68f3f58
CrankyCranton <crantonbogsmorehooversil@gmail.com>
2023-07-12 19:53:58 -0400

    Swat don't attack NPC(s)

--- characters/enemies/swat/swat.gd
+++ characters/enemies/swat/swat.gd
@@ -6,6 +6,6 @@ export var dmg := 1
 
 
 func _on_SwatZone_area_entered(area: Area2D) -> void:
-	if area.owner.type == type:
+	if area.owner.type == type or area.owner.type == "all":
 		return
 	area.take_dmg(global_position, dmg)
--- ui/screens/shop.gd
+++ ui/screens/shop.gd
@@ -1,2 +1,51 @@
 class_name Shop
 extends Control
+
+
+export var min_items := 5
+export var max_items := 15
+export var items: Array = [
+	ItemInfo.new(preload("res://pickups/ammo_pickup.png"), 1000, "res://guns/pistol/pistol.png"),
+
+]
+
+onready var tabs: TabContainer = $Tabs
+onready var shop: VBoxContainer = tabs.get_node("Shop")
+onready var money: Label = shop.get_node("TopBar/Money")
+onready var dressing_room: Control = tabs.get_node("Dressing Room")
+
+
+class ItemInfo:
+	var icon: Texture
+	var price: int
+	var scene: String
+
+
+# warning-ignore:shadowed_variable
+# warning-ignore:shadowed_variable
+# warning-ignore:shadowed_variable
+	func _init(icon: Texture, price: int, scene: String) -> void:
+		self.icon = icon
+		self.price = price
+		self.scene = scene
+
+
+func _ready() -> void:
+	fill_shop()
+	set_money(Inventory.money)
+# warning-ignore:return_value_discarded
+	Inventory.connect("money_changed", self, "set_money")
+
+
+# warning-ignore:shadowed_variable
+func set_money(money: int) -> void:
+	self.money.text = str(money)
+
+
+func fill_shop() -> void:
+	for i in rand_range(min_items, max_items):
+		add_item()
+
+
+func add_item() -> void:
+	pass
