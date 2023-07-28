e8741027f433a53838a95f26056a503f95dd58d3
CrankyCranton <crantonbogsmorehooversil@gmail.com>
2023-07-28 19:10:10 -0400

    Fix red detectors

--- levels/red_detector/red_detector.gd
+++ levels/red_detector/red_detector.gd
@@ -2,6 +2,8 @@ class_name RedDetector
 extends Area2D
 
 
+export var dmg := 1
+
 var detecting := true setget _on_detecting_set
 
 onready var sprite: Sprite = $Sprite
@@ -10,13 +12,14 @@ onready var collision_shape: CollisionShape2D = $CollisionShape2D
 
 func _on_detecting_set(value: bool) -> void:
 	detecting = value
-	monitorable = true
-	monitoring = false
 
 	sprite.visible = detecting
 	collision_shape.set_deferred("disabled", not detecting)
 
+
 func _on_Timer_timeout() -> void:
 	self.detecting = not detecting
-	monitorable = false
-	monitoring = false
+
+
+func _on_red_detector_area_entered(area: HitBox) -> void:
+	area.take_dmg(global_position, dmg)
