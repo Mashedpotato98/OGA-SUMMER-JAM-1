4377fecc24a575ecea20dcda347b2eaf08a4f9d0
CrankyCranton <crantonbogsmorehooversil@gmail.com>
2023-07-29 13:51:43 -0400

    Fix ammo pickup

--- pickups/ammo_pickup.gd
+++ pickups/ammo_pickup.gd
@@ -2,9 +2,10 @@ class_name AmmoPickup
 extends Pickup
 
 
-export var amount := 10
-
-
 func _collect(body: Node) -> void:
-	body.ammo += amount
+	var type: String = (body as Robber).item.filename
+	if type == Inventory.BRIBE_PATH:
+		return
+
+	Inventory.items[type] += int(ceil(Inventory.items_list[type].ammo / 2.0))
 	queue_free()
