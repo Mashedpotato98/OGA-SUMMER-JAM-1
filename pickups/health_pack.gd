4601918050bb3919397b72026d6f582072917a63
CrankyCranton <crantonbogsmorehooversil@gmail.com>
2023-07-29 14:39:22 -0400

    Change health pickup

--- pickups/health_pack.gd
+++ pickups/health_pack.gd
@@ -1,9 +1,11 @@
 class_name HealthPickup
 extends Pickup
 
+
 export var health_amount = 1
 
-func _collect(_body: Node) -> void:
-	if Robber.hp < 3:
-		Robber.hp += health_amount
+
+func _collect(body: Node) -> void:
+	if body.hp < body.max_hp:
+		body.hp += health_amount
 	queue_free()
