918e245f918e1b634a7666414abaa3f6298ee27a
CrankyCranton <crantonbogsmorehooversil@gmail.com>
2025-03-31 22:24:56 -0400

    Reformat pause_screen.gd

--- ui/screens/pause_screen.gd
+++ ui/screens/pause_screen.gd
@@ -8,14 +8,26 @@ class_name PauseScreen extends CanvasLayer
 #endregion
 
 
+#region Functions
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("ui_cancel") and not get_tree().paused:
+	if event.is_action_pressed(&"ui_cancel") and not get_tree().paused:
 		get_tree().paused = true
 		interface.show()
 		bg.show()
 		resume_button.grab_focus()
 
 
+#region Custom
+func enable() -> void:
+	set_process_input(true)
+
+
+func disable() -> void:
+	set_process_input(false)
+#endregion
+
+
+#region Events
 func _on_ResumeButton_pressed() -> void:
 	bg.hide()
 	interface.hide()
@@ -24,11 +36,5 @@ func _on_ResumeButton_pressed() -> void:
 
 func _on_QuitButton_pressed() -> void:
 	get_tree().quit()
-
-
-func enable() -> void:
-	set_process_input(true)
-
-
-func disable() -> void:
-	set_process_input(false)
+#endregion
+#endregion
